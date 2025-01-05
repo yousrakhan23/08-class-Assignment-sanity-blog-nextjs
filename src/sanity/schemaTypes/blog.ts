232806088de701e709603562a04de72069fb2f23
yousrakhan23 <ammarahkhan2016@gmail.com>
2025-01-05 19:43:24 +0500

    sanity

---
+++ src/sanity/schemaTypes/blog.ts
@@ -0,0 +1,36 @@
+import { defineField, defineType } from "sanity";
+
+export const Blog = defineType({
+  name: "blog",
+  title: "Blog",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "text",
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 200,
+      },
+    }),
+    defineField({
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    }),
+  ],
+});
---
+++ src/sanity/schemaTypes/index.ts
@@ -0,0 +1,6 @@
+import { type SchemaTypeDefinition } from 'sanity'
+import { Blog } from './blog'
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [Blog],
+}
